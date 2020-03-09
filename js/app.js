27778a236da5a83fa0346e1648a1724125b5caca
racarter1215 <racarter1215@gmail.com>
2020-03-09 13:17:48 -0700

    initial changes to html, js, and css

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+Math.floor(Math.random() * 10 + 1)
+
+var store1 = {
+    name: 'Seattle'
+    minimumHours: Math.floor(Math.random() * 10 - 5)
+    maximumHours: Math.ceil(Math.random() * 10 + 10)
+    averageSold: Math.random(maximumHours - minimumHours))
+    storeHours: ["minimum", "max", "average"],
+    sales: function () {
+
+    }
+}
